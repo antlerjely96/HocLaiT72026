7f71d2d67b99879ac9bcf666815aa69912d1d907
antlerjely96 <antlerjely96@gmail.com>
2026-07-07 11:56:50 +0700

    Mảng (array)

--- HocLaiT72026/Array.cs
+++ HocLaiT72026/Array.cs
@@ -42,5 +42,107 @@ public class Array
             Console.Write(arr[i] + "\t");
         }
         Console.WriteLine();
+        
+        /* 2. Nhập số nguyên a từ bàn phím.
+            Nhập và hiển thị mảng số thực có a phần tử.
+        */
+        //Khai báo và nhập a
+        int a;
+        Console.Write("a = ");
+        a = int.Parse(Console.ReadLine());
+        //Khai bao mang
+        double[] arr2 = new double[a];
+        //Nhap mang
+        for (i = 0; i < a; i++)
+        {
+            Console.Write("arr2[" + i + "] = ");
+            arr2[i] = double.Parse(Console.ReadLine());
+        }
+        //Hien thi
+        Console.Write("Mang vua nhap: ");
+        for (i = 0; i < a; i++)
+        {
+            Console.Write(arr2[i] + "\t");
+        }
+        Console.WriteLine();
+        
+        /* 3. Nhap mang so nguyen co b phan tu (b nhap tu ban phim)
+            Tinh tong cac phan tu trong mang
+        */
+        //Khai báo và nhập b
+        int b;
+        Console.Write("b = ");
+        b = int.Parse(Console.ReadLine());
+        //Khai bao mang
+        int[] arr3 = new int[b];
+        //Nhap mang
+        for (i = 0; i < b; i++)
+        {
+            Console.Write("arr3[" + i + "] = ");
+            arr3[i] = int.Parse(Console.ReadLine());
+        }
+        //Tính
+        int tong = 0;
+        for (i = 0; i < b; i++)
+        {
+            tong += arr3[i];
+        }
+        Console.WriteLine("Tong cac phan tu cua mang: " + tong);
+        
+        /* 4. Nhap mang so nguyen co d phan tu.
+         Tinh tich cac phan tu chia het cho 3 hoac 5 */
+        //Khai báo và nhập b
+        int d;
+        Console.Write("d = ");
+        d = int.Parse(Console.ReadLine());
+        //Khai bao mang
+        int[] arr4 = new int[d];
+        //Nhap mang
+        for (i = 0; i < d; i++)
+        {
+            Console.Write("arr4[" + i + "] = ");
+            arr4[i] = int.Parse(Console.ReadLine());
+        }
+        //Tinh
+        int tong2 = 0;
+        for (i = 0; i < d; i++)
+        {
+            if (arr4[i] % 3 == 0 || arr4[i] % 5 == 0)
+            {
+                tong2 += arr4[i];
+            }
+        }
+        Console.WriteLine("Tong cac phan tu chia het cho 3 hoac 5: " + tong2);
+        
+        /* 5. Nhap mang so thuc co m phan tu.
+            Tim gia tri nho nhat trong mang.
+        */
+        //Khai bao va nhap m
+        int m;
+        Console.Write("m = ");
+        m = int.Parse(Console.ReadLine());
+        //Khai bao mang
+        double[] arr5 = new double[m];
+        //Nhap mang
+        for (i = 0; i < m; i++)
+        {
+            Console.Write("arr5[" + i + "] = ");
+            arr5[i] = double.Parse(Console.ReadLine());
+        }
+        //Tim max
+        double max = arr5[0];
+        for (i = 0; i < m; i++)
+        {
+            if (max < arr5[i])
+            {
+                max = arr5[i];
+            }
+        }
+        //Hien thi
+        Console.WriteLine("Phan tu co gia tri lon nhat trong mang: " + max);
+        
+        /*
+            6. Nhap mang so thuc co x phan tu. Sap xep mang theo chieu tang dan        
+        */
     }
 }
